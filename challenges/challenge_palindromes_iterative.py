0f3e24f20a55a5cd98e594a669660c2ce084ed8c
João Victor Pistorio <joao.pistorio@gmail.com>
2022-02-10 21:03:48 -0300

    requirement 5 search source

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,3 +2,6 @@ def is_palindrome_iterative(word):
     if not word:
         return False
     return word == word[::-1]
+
+# https://stackoverflow.com/questions/40580558/why-is-word-word-1-to-test-for-palindrome-faster-than-a-more-algorithmi
+# https://stackoverflow.com/questions/509211/understanding-slice-notation

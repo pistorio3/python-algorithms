3ff6e8e1e98670bd99fdb57e7526e52e5848dbbb
João Victor Pistorio <joao.pistorio@gmail.com>
2022-02-10 21:34:17 -0300

    requirement 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,43 @@
+def merge(arr, L, R):
+    i = j = k = 0
+    while i < len(L) and j < len(R):
+        if L[i] < R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+    while i < len(L):
+        arr[k] = L[i]
+        i += 1
+        k += 1
+    while j < len(R):
+        arr[k] = R[j]
+        j += 1
+        k += 1
+    return arr
+
+
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        mergeSort(L)
+        mergeSort(R)
+
+        arr = merge(arr, L, R)
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. ok"""
+    if len(first_string) != len(second_string):
+        return False
+    first = list(first_string)
+    second = list(second_string)
+    mergeSort(first)
+    mergeSort(second)
+    return first == second
+
+# https://www.geeksforgeeks.org/merge-sort/
